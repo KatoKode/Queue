e6b151f5d247c1ea3d5f7a4c63f6fb91443fef54
Data Structures and Algorithms in C and Assembly Language <mcintosh1798@gmail.com>
2025-07-06 21:48:24 -0400

    Update go_qtest.sh

--- qtest/go_qtest.sh
+++ qtest/go_qtest.sh
@@ -17,10 +17,6 @@
 #   with this program; if not, write to the Free Software Foundation, Inc.,
 #   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #-------------------------------------------------------------------------------
-# file:   go_qtest.sh
-# author: J. McIntosh
-# brief:  run script for Queue demo program
-#-------------------------------------------------------------------------------
 #
 echo -e "\nRunning ./qtest"
 rnd=`shuf -i 10000000-99999999 -n 1`

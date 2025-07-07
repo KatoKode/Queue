26f2dd7639b94c8cb9a093863558913d719113c7
Data Structures and Algorithms in C and Assembly Language <mcintosh1798@gmail.com>
2025-07-06 21:49:33 -0400

    Update queue_make.sh

--- queue_make.sh
+++ queue_make.sh
@@ -16,8 +16,7 @@
 #   with this program; if not, write to the Free Software Foundation, Inc.,
 #   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #-------------------------------------------------------------------------------
-# btree_make.sh
-#
+# !/bin/sh
 clear;
 
 sep="--------------------------------------------------------------------------------"

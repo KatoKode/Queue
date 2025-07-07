3b44832e82dc2e92567942bc688ce66af0d927d1
Data Structures and Algorithms in C and Assembly Language <mcintosh1798@gmail.com>
2025-07-06 21:50:02 -0400

    Update queue_make.sh

--- queue_make.sh
+++ queue_make.sh
@@ -1,5 +1,5 @@
 #-------------------------------------------------------------------------------
-#   B-Tree Implementation in x86_64 Assembly Language with C Interface
+#   Queue Implementation in x86_64 Assembly Language with C Interface
 #   Copyright (C) 2025  J. McIntosh
 #
 #   This program is free software; you can redistribute it and/or modify
